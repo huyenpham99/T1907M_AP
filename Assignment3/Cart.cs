0af8a2e319b1e20c9144aefc8b2638adb1e23854
huyenpham99 <53186394+huyenpham99@users.noreply.github.com>
2020-04-21 23:08:02 +0700

    Assignment3

---
+++ Assignment3/Cart.cs
@@ -0,0 +1,112 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using Demo.Lab2;
+
+namespace Demo.Assignment3
+{
+   public class Cart : Product
+    {
+        public string _iD;
+        public string _customer;
+        public double _grantTotal;
+        public string city;
+        public string country;
+        public List<Product> products = new List<Product>();
+        public List<double> totals = new List<double>();
+
+        public void AddProduct(Product product)
+        {
+            products.Add(product);
+        }
+
+        public Cart()
+        {
+        }
+
+        public Cart(string iD, string customer, double grantTotal, string city, string country)
+        {
+            _iD = iD;
+            _customer = customer;
+            _grantTotal = grantTotal;
+            this.city = city;
+            this.country = country;
+        }
+
+        public Cart(int id, string name, double price, int qty, string image, string desc, string iD, string customer, double grantTotal, string city, string country) : base(id, name, price, qty, image, desc)
+        {
+            _iD = iD;
+            _customer = customer;
+            _grantTotal = grantTotal;
+            this.city = city;
+            this.country = country;
+        }
+
+        public void showInfo()
+                 {
+                     
+                     Console.WriteLine("======Customer information======= ");
+                     Console.WriteLine("Customer id: " + _iD + " | " + "Name: " + _customer + " | " + "City: " + city + " | " + "Country: " + country);
+                     foreach (Product p in products)
+                     {
+                         Console.WriteLine("ID: " + p._id + " | " + "Name: " + p._name + " | " + "Price: " + p._price + " | " + "Qty: " + p._qty + " | " + "Image: " + p._image + 
+                                           " | " + "Desc: " + p._desc);
+                     }
+                 }
+
+        public void RemoveProduct()
+        {
+            string removeName;
+            Console.WriteLine("Nhap san pam muon xoa: ");
+            removeName = Convert.ToString(Console.ReadLine());
+            for (int i = 0; i < products.Count; i++)
+            {
+                if (removeName.Equals(productList[i]._name))
+                {
+                    products.RemoveAt(i);
+                    Console.WriteLine("Da xoa thanh cong");
+                }
+                else
+                {
+                    Console.WriteLine("Khong co ten cua san pham tren!");
+                }
+            }
+        }
+
+        public void grandTotalCal()
+        {
+            foreach (Product p in products)
+            {
+                _grantTotal = p._price * p._qty;
+                totals.Add(_grantTotal);
+            }
+            
+
+            double fee;
+            if (country == "vietnam")
+            {
+                if (city == "HN" | city =="HCM")
+                {
+                    fee = _grantTotal * 0.01;
+                    Console.WriteLine("Tong tien chua bao gom phi ship: " + _grantTotal);
+                    Console.WriteLine("Phi ship:  " +( fee));
+                    Console.WriteLine("Tong tien:" + (_grantTotal + fee));
+                }
+                else
+                {
+                    fee = _grantTotal * 0.02;
+                    Console.WriteLine("Tong tien chua bao gom phi ship: " + _grantTotal);
+                    Console.WriteLine("Phi ship:  " +( fee));
+                    Console.WriteLine("Tong tien:" + (_grantTotal + fee));
+                }
+            }
+            else
+            {
+                fee = _grantTotal * 0.05;
+                Console.WriteLine("Tong tien chua bao gom phi ship: " + _grantTotal);
+                Console.WriteLine("Phi ship:  " +( fee));
+                Console.WriteLine("Tong tien:" + (_grantTotal + fee));
+            }
+
+        }
+    }
+}
---
+++ Assignment3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Demo.Lab2;
+
+namespace Demo.Assignment3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int _id;
+            string _name;
+            double _price;
+            int _qty;
+            string _image;
+            string _desc;
+         
+         Cart cart = new Cart("1", " Huyen ", 1, "HN", "vietnam");
+         Product product = new Product();
+         Console.WriteLine("Enter id: ");
+         _id = Convert.ToInt32(Console.ReadLine());
+         Console.WriteLine("Enter name: ");
+         _name = Convert.ToString(Console.ReadLine());
+         Console.WriteLine("Enter price: ");
+         _price = Convert.ToDouble(Console.ReadLine());
+         Console.WriteLine("Enter qty: ");
+         _qty = Convert.ToInt32(Console.ReadLine());
+         Console.WriteLine("Enter image: ");
+         _image = Convert.ToString(Console.ReadLine());
+         Console.WriteLine("Enter desc: ");
+         _desc = Convert.ToString(Console.ReadLine());
+         cart.AddProduct(new Product(_id, _name,_price, _qty, _image, _desc));
+         cart.showInfo();
+         cart.grandTotalCal();
+         
+        }
+    }
+}
